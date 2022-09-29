fe2706656024b83cdf8f0ae42ba6bda30be96bae
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-29 11:53:51 -0500

    corrected list david already did it in list itself

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,7 @@
 import React from "react";
-import axios from "axios";
 import "./App.css";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 
 function App() {
-  const dispatch = useDispatch();
-
-  function getPizzaList() {
-    axios
-      .get("/api/pizza")
-      .then((result) => {
-        console.log("result.data :>> ", result.data);
-        dispatch({ type: "ADD_PIZZA_LIST", payload: result.data });
-      })
-      .catch((err) => {
-        console.log("error caught in GET PIZZA LIST :>> ", err);
-      });
-  }
-
-  useEffect(() => {
-    getPizzaList();
-  }, []);
   return (
     <div className="App">
       <header className="App-header">
