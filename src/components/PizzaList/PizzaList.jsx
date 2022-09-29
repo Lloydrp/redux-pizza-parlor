9889cc04d28379b8880e5fcd0eb70ce42df3696b
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-29 11:46:49 -0500

    added axios get request that dispatches database data to a reducer

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,5 +1,29 @@
+import axios from 'axios';
+import {useSelector, useDispatch} from 'react-redux';
+
 function PizzaList() {
-  return <h2>PizzaList</h2>;
+  // declare useDispatch.
+  const dispatch = useDispatch();
+
+  // get the array of pizzas and send them to the reducer.
+  axios.get('/api/pizza')
+  .then((response) => {
+    console.log('axios GET for /api/pizza');
+    // dispatch all of the za to the reducer.
+    dispatch({
+      type: 'ADD_PIZZA_LIST',
+      payload: response.data
+    });
+  })
+  .catch(err => console.log('Error getting za', err));
+
+  
+  // pizzaList = useSelector(store => store.pizzaListReducer);
+
+  return (
+    <h2>PizzaList</h2>
+  
+  );
 }
 
 export default PizzaList;
