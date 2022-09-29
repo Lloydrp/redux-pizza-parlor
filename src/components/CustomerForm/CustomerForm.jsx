8b31488d8b5361c72ff02d80029100945d2eeaf8
Lee Baesler <lee.baesler@gmail.com>
2022-09-29 12:00:57 -0500

    form completed

--- src/components/CustomerForm/CustomerForm.jsx
+++ src/components/CustomerForm/CustomerForm.jsx
@@ -1,5 +1,39 @@
-function CustomerForm() {
-  return <h2>CustomerForm</h2>;
+import { useState } from 'react'
+
+function CustomerForm({addCustomer}) {
+  const [fullName, setFullName] = useState('');
+  const [streetAddress, setStreetAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [zipCode, setZipCode] = useState('');
+ 
+ const handleSubmit =(event) => {
+  event.preventDefauly();
+ }
+  return (
+    <form>
+      <input 
+        placeholder="Full Name" 
+        value={fullName} 
+        onChange={(evt) => setFullName(evt.target.value)} 
+      />
+      <input 
+        placeholder="Address" 
+        value={streetAddress} 
+        onChange={(evt) => setSteetAddress(evt.target.value)} 
+      />
+      <input 
+        placeholder="City" 
+        value={city} 
+        onChange={(evt) => setCity(evt.target.value)} 
+      />
+      <input 
+        placeholder="Job Title" 
+        value={zipCode} 
+        onChange={(evt) => setZipCode(evt.target.value)}
+      /> 
+      <input type="submit" value="Submit" />
+      </form>
+  );
 }
 
 export default CustomerForm;
