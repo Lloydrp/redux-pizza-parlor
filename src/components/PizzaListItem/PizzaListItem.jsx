c32c229ddc084e0b54668bbbde6bb959f75b122e
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-29 13:20:20 -0500

    added a local state to track if the za was added to the cart

--- src/components/PizzaListItem/PizzaListItem.jsx
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
+
 function PizzaListItem({za}) {
+  const [added, setAdded] = useState(false);
+
   return (
-    <div>
+    <div className="za-container">
       <p>{za.name}</p>
       <img src={za.image_path} alt="Just a spicy pic of some za."/>
+      <p>{za.description}</p>
+      <p>{za.price}</p>
+      <button></button>
     </div>
   )
 }
