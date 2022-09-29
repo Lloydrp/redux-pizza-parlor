d7809ebb650c663e34a5e84c5c30a9dcca965221
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-29 14:59:00 -0500

    added router and temporary nav bar for testing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../Header/Header";
 import PizzaList from "../PizzaList/PizzaList";
+import { HashRouter as Router, Route, NavLink, Link, Switch } from "react-router-dom";
 import "./App.css";
 import CustomerForm from "../CustomerForm/CustomerForm.jsx";
 
@@ -8,8 +9,28 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CustomerForm />
-      <PizzaList />
+      <Router>
+        <div className="nav-bar">
+          <NavLink to="/">Za Home</NavLink>{" "}
+          <NavLink to="/info">Your Info</NavLink>{" "}
+          <NavLink to="/checkout">Checkout</NavLink>{" "}
+          <NavLink to="/admin">Za Admin</NavLink>
+        </div>
+        <Switch>
+          <Route path="/info">
+            <CustomerForm />
+          </Route>
+          <Route path="/checkout">
+            {/* insert checkout component here */}
+          </Route>
+          <Route path="/admin">
+            {/* insert admin component here */}
+          </Route>
+          <Route path="/">
+            <PizzaList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
