85ff5810b6fd029787415c3b02b9d890ea704a1b
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-29 11:51:29 -0500

    added useEffect

--- src/components/PizzaList/PizzaList.jsx
+++ src/components/PizzaList/PizzaList.jsx
@@ -1,28 +1,37 @@
 import axios from 'axios';
+import { useEffect } from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
 function PizzaList() {
   // declare useDispatch.
   const dispatch = useDispatch();
 
-  // get the array of pizzas and send them to the reducer.
-  axios.get('/api/pizza')
-  .then((response) => {
-    console.log('axios GET for /api/pizza');
-    // dispatch all of the za to the reducer.
-    dispatch({
-      type: 'ADD_PIZZA_LIST',
-      payload: response.data
-    });
-  })
-  .catch(err => console.log('Error getting za', err));
+  const pizzaList = useSelector(store => store.pizzaListReducer);
 
-  
-  // pizzaList = useSelector(store => store.pizzaListReducer);
+
+  // get the array of pizzas and send them to the reducer.
+  const fetchZa = () => {
+    axios.get('/api/pizza')
+    .then((response) => {
+      console.log('axios GET for /api/pizza');
+      // dispatch all of the za to the reducer.
+      dispatch({
+        type: 'ADD_PIZZA_LIST',
+        payload: response.data
+      });
+    })
+    .catch(err => console.log('Error getting za', err));
+  }
+
+  useEffect(() => {
+    fetchZa();
+  }, []);
 
   return (
-    <h2>PizzaList</h2>
-  
+    <section>
+      <h2>Step 1: Get Your Za Broski!</h2>
+      {pizzaList.map()}
+    </section>
   );
 }
 
