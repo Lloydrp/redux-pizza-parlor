303f32a84d4bc4d1f13d72afae9a1362c7df97aa
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-29 13:41:54 -0500

    added comments and dispatch

--- src/components/PizzaListItem/PizzaListItem.jsx
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -1,8 +1,40 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
 
 
 function PizzaListItem({za}) {
-  const [added, setAdded] = useState(false);
+  // local use state to check if a za item was added to cart.
+  const [added, setAdded] = useState(true); //defualt state is true before re-render.
+  const dispatch = useDispatch();
+
+  // run functions on click of add/remove button.
+  const handleClick = () => {
+    // set added to the inverse of itself.
+    setAdded(!added);
+    console.log(added); // current state of added at this point.
+    // set up the object to be dispatched.
+    const newZa = {
+      id: za.id,
+      quantity: za.quantity
+    }
+    // handle dispatch based on boolean added.
+    if(added === true) {
+      // payload of the id of the za to be added and
+      // the quantity of the za.
+      dispatch({
+        type: 'ADD_PIZZA_ORDER',
+        payload: newZa
+      });
+    } else if (added === false) {
+      // payload of the id of the za to be removed.
+      dispatch({
+        type: 'REMOVE_PIZZA_ORDER',
+        payload: za.id
+      });
+    }
+  }
+
+  // console.log(added);
 
   return (
     <div className="za-container">
@@ -10,7 +42,7 @@ function PizzaListItem({za}) {
       <img src={za.image_path} alt="Just a spicy pic of some za."/>
       <p>{za.description}</p>
       <p>{za.price}</p>
-      <button></button>
+      <button onClick={() => handleClick()}>{added ? 'Add' : 'Remove'}</button>
     </div>
   )
 }
