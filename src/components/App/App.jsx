a633dff1f37f09b8fbaafe0690ac149db841b886
Lee Baesler <lee.baesler@gmail.com>
2022-09-29 15:10:03 -0500

    revised

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "../Header/Header";
+import CustomerDetails from "../Pages/CustomerDetails";
 import PizzaList from "../PizzaList/PizzaList";
 import "./App.css";
-import CustomerForm from "../CustomerForm/CustomerForm.jsx";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CustomerForm />
+      <CustomerDetails  />
       <PizzaList />
     </div>
   );
