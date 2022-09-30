fb9e7518528a77769e03cae9f97f04cefb24233a
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-30 10:35:23 -0500

    working admin GET

--- src/components/Pages/Admin.jsx
+++ src/components/Pages/Admin.jsx
@@ -1,15 +1,26 @@
-import { useSelector } from "react-redux";
+import axios from "axios";
+import { useState } from "react";
 import Header from "../Header/Header";
 
 function Admin() {
-  const adminList = useSelector((store) => store.admin);
+  const [adminOrders, setAdminOrders] = useState([]);
+
+  axios
+    .get("/api/order")
+    .then((result) => {
+      setAdminOrders(result.data);
+    })
+    .catch((error) => {
+      console.log("error caught in order get :>> ", error);
+    });
   return (
     <>
       <Header total={false} />
       <h2>AdminList Below:</h2>
       <div>
-        {adminList.map(
-          (item) => `${item.customer_name} ${item.time} ${item.type} ${item.total}`
+        {adminOrders.map(
+          (item) =>
+            `${item.customer_name} ${item.time} ${item.type} ${item.total}`
         )}
       </div>
     </>
