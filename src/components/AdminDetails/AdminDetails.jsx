facdd48ab0fe06c0a503398f0992720de08164c7
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-30 12:02:59 -0500

    working admin popdown

--- src/components/AdminDetails/AdminDetails.jsx
+++ src/components/AdminDetails/AdminDetails.jsx
@@ -1,11 +1,36 @@
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
 function AdminDetails({ item }) {
-  console.log("item :>> ", item);
+  const [adminDetails, setAdminDetails] = useState([]);
+  const pizzaList = useSelector((store) => store.pizzaList);
+
+  function getAdminDetails() {
+    axios
+      .get("/api/order/lineitem")
+      .then((result) => {
+        setAdminDetails(result.data);
+        console.log("result.data :>> ", result.data);
+      })
+      .catch((error) => {
+        console.log("error caught in get lineitems :>> ", error);
+      });
+  }
+  console.log("pizzaList :>> ", pizzaList);
+
+  useEffect(() => {
+    getAdminDetails();
+  }, []);
   return (
     <div>
-      {item.street_address} {city} {zip}
+      {item.street_address} {item.city} {item.zip}
       <div>Pizzas Ordered:</div>
-      {item.pizzas.map((pizza) => (
-        <li>{pizza.id}</li>
+      {adminDetails.map((pizza) => (
+        <li key={pizza.id}>
+          {pizza.order_id === item.id ? pizzaList[pizza.pizza_id - 1].name : ""}
+        </li>
       ))}
     </div>
   );
