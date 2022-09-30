5a9d1aa3a112ca6ee02bf8161e8872ceeaf2f3bb
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-30 11:09:39 -0500

    added pizza list popdown

--- src/components/Pages/OrderPage.jsx
+++ src/components/Pages/OrderPage.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Header from "../Header/Header";
 import PizzaList from "../PizzaList/PizzaList";
+import PizzaOrderModal from "../PizzaOrderModal/PizzaOrderModal";
 
 function OrderPage() {
+  const [isHovering, setIsHovering] = useState(false);
   return (
     <>
-      <Header total={true} />
+      <Header total={true} setIsHovering={setIsHovering} />
+      {isHovering && <PizzaOrderModal />}
       <PizzaList />
     </>
   );
