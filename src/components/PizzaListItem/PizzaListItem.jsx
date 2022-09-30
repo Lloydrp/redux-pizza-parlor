e1c1f8423ebf4d8fb690e1eabbbf419521116244
David Lindberg <david.wesley.lindberg@gmail.com>
2022-09-30 11:16:33 -0500

    added a za-item-textbox

--- src/components/PizzaListItem/PizzaListItem.jsx
+++ src/components/PizzaListItem/PizzaListItem.jsx
@@ -42,10 +42,12 @@ function PizzaListItem({ za }) {
 
   return (
     <div className="za-item-container">
-      <p>{za.name}</p>
       <img src={za.image_path} alt="Just a spicy pic of some za." />
-      <p>{za.description}</p>
-      <p>{za.price}</p>
+      <div className="za-item-textbox">
+        <p>{za.name}</p>
+        <p>{za.description}</p>
+        <p>{za.price}</p>
+      </div>
       <button onClick={() => handleClick()}>{added ? "Add" : "Remove"}</button>
     </div>
   );
